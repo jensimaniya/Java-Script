871166516877994474912b446118bf6057a11d49
jensimaniya <jensimaniya@gmail.com>
2025-01-27 17:42:36 +0530

    Java Script

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log("start");
+
+var a = 20;
+let b = 30;
+b = 50;
+const c = 200;
+console.log(a + b + c);
+
+let x = 100;
+let y = "200";
+let z = "false";
+let p = true;
+console.log(typeof x, typeof y, typeof z, typeof p);
+
+let m = "100";
+let n = "200";
+const o = "300";
+console.log(m + n + o);
+
+let j = "10";
+const e = "20";
+let s = 10;
+console.log(j + e - s);
+console.log(Number(j) + Number(e) - s);
