53c9d567d56e2c35195053bb2ae40c7cc6cea7cf
jensimaniya <jensimaniya@gmail.com>
2025-02-03 17:42:22 +0530

    Loop

---
+++ Loop/MinMax.js
@@ -0,0 +1,7 @@
+let max = 19;
+let min = 14;
+for (let i = min; i < max; i++)
+{
+    console.log(i);
+    
+}
---
+++ Loop/R&W.js
@@ -0,0 +1,5 @@
+for (let num = 1; num <= 5; num++)
+{
+    console.log("RED and WHITE");
+    
+}
