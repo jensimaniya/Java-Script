e00adf401d6ed9dceb40f7808d16f51a565c7340
jensimaniya <jensimaniya@gmail.com>
2025-02-04 17:25:29 +0530

    Nested Loop"

---
+++ Loop/Do-WhileLoop.js
@@ -0,0 +1,14 @@
+let i = 1;
+do {
+  console.log(i);
+  i++;
+} while (i < 10);
+
+console.log("----------");
+
+
+let j = 10;
+do {
+  console.log(j);
+  j++;
+} while (j < 10);
---
+++ Loop/Nested Loop/Q-3.js
@@ -0,0 +1,8 @@
+for (let i = 1; i <= 5; i++)
+{
+    for (let j = i; j <= 3; j++)
+    {
+        console.log(i+j);
+        
+    }
+}
