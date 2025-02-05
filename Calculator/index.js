092958d46c9aea9a16564cb1e120c0f7437ce0b8
jensimaniya <jensimaniya@gmail.com>
2025-02-05 19:20:57 +0530

    Calc

---
+++ Calculator/index.js
@@ -0,0 +1,26 @@
+function sum() {
+  let a = document.getElementById("valueOfA").value;
+  let b = document.getElementById("valueOfB").value;
+  // document.getElementById("result").innerHTML = Number(a) + Number(b);
+  document.getElementById("result").innerHTML = `sum of a : ${a} and b:${b}=${
+    Number(a) + Number(b)
+  }`;
+}
+
+function sub() {
+  let a = document.getElementById("valueOfA").value;
+  let b = document.getElementById("valueOfB").value;
+  document.getElementById("result").innerHTML = Number(a) - Number(b);
+}
+
+function mul() {
+  let a = document.getElementById("valueOfA").value;
+  let b = document.getElementById("valueOfB").value;
+  document.getElementById("result").innerHTML = Number(a) * Number(b);
+}
+
+function div() {
+  let a = document.getElementById("valueOfA").value;
+  let b = document.getElementById("valueOfB").value;
+  document.getElementById("result").innerHTML = Number(a) / Number(b);
+}
