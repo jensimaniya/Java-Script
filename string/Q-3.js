646b5f505ade12bf4c203212793a18a496ea8257
jensimaniya <jensimaniya@gmail.com>
2025-02-07 18:17:51 +0530

    js

--- string/Q-3.js
+++ string/Q-3.js
@@ -1,2 +1,9 @@
-let string = "white";
-console.log(string.toUpperCase());
+// let string = "white";
+// console.log(string.toUpperCase());
+
+function UpperValue() {
+    let string = document.getElementById("textValue").value;
+
+    document.getElementById("result").innerHTML = string.toUpperCase();
+
+}
