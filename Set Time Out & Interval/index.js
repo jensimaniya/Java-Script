95c60ecc961ffbf3814de6ec23001ac8615b8208
jensimaniya <jensimaniya@gmail.com>
2025-02-25 17:20:22 +0530

    Set Time Out && Set Interval

---
+++ Set Time Out & Interval/index.js
@@ -0,0 +1,19 @@
+setTimeout(() => {
+  console.log("TimeOut");
+}, 5000);
+
+setInterval(() => {
+    console.log("jensi");
+
+}, 2000);
+
+console.log("a");
+console.log("b");
+console.log(1);
+
+setTimeout(() => {
+  console.log("ABC");
+}, 2000);
+
+console.log(2);
+
---
+++ Set Time Out & Interval/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+//   console.log("TimeOut");
+// }, 5000);
+
+
+// setInterval(() => {
+//     console.log("jensi");
+    
+// }, 2000);
