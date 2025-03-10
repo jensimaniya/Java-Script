e244d464ea0a7dcb4f2c8c2965632af8b9ce29f1
jensimaniya <jensimaniya@gmail.com>
2025-03-10 15:12:02 +0530

    Live Form Validation

--- Live Form Validation/index.js
+++ Live Form Validation/index.js
@@ -13,37 +13,27 @@ const handleUserData = (e) => {
 
 document.getElementById("userData").addEventListener("submit", handleUserData);
 
-
-
-
 // live validation
 
-
-
-
 document.querySelector("#username").addEventListener("input", (e) => {
-  let username = e.target.value;
+  let username = e.target.value; //getValue("username");
   let usernameRegex = /^[a-zA-Z\-]+$/;
 
-  
   if (usernameRegex.test(username)) {
-    
     document.getElementById("username").setAttribute("class", "paased");
   } else {
     document.getElementById("username").setAttribute("class", "err");
   }
 });
 
-
 const validation = (id, type) => {
   document.getElementById(id).setAttribute("class", type);
 };
 
-
 document.querySelector("#password").addEventListener("input", (e) => {
   let password = e.target.value;
   let passwordRegex =
-    /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$%&? "])[a-zA-Z0-9!#$%&@?]{8,20}$/;
+    /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$@%&? "])[a-zA-Z0-9!#$%&@?]{8,20}$/;
 
   if (passwordRegex.test(password)) {
     validation("password", "paased");
