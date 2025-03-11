505eeac1578b2aeeb078d0702ce996655954e317
jensimaniya <jensimaniya@gmail.com>
2025-03-11 17:32:35 +0530

    js

--- Error Handling/index.js
+++ Error Handling/index.js
@@ -1,30 +1,20 @@
 const divide = (a, b) => {
-    if (b == 0) {
-        throw new Error("error :cannot divided by zero");
-    }
-    
-   return a/b 
-}
+  if (b == 0) {
+    throw new Error("error :cannot divided by zero");
+  }
+
+  return a / b;
+};
 console.log("before divide");
 try {
-    console.log(divide(10, 2));
-    let i = 0;
-    // finally ni process bandh karva mate
-    while (i < 10) {
-        
-    }
-
-}
-catch (err)
-{
-    console.log(err.message);
-    
-}
-finally {
-    console.log("process finished....");
-    
+  console.log(divide(10, 2));
+  let i = 0;
+  // finally ni process bandh karva mate
+  while (i < 10) {}
+} catch (err) {
+  console.log(err.message);
+} finally {
+  console.log("process finished....");
 }
 
 console.log("after Divide");
-
-
